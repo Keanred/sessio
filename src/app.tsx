6be7cbfbda352ec2cbd2f361ffd455f63b09bfe3
Keanred <47241653+Keanred@users.noreply.github.com>
2026-04-16 21:54:54 +0300

    feat: initial project structure

---
+++ src/app.tsx
@@ -0,0 +1,6 @@
+/** @jsx React.createElement */
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const root = createRoot(document.body);
+root.render(<h2>Hello from React!</h2>);
